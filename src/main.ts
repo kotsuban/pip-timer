a10eb16c711837b09b3ebdfb1110fb422cdc7d05
kotsuban <antontamazlukar@gmail.com>
2025-06-16 14:32:06 +0200

    Update buttons tooltip

--- src/main.ts
+++ src/main.ts
@@ -14,10 +14,10 @@ import { INITIAL_TIME } from "./store";
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <header>
     <div class="btn-group">
-      <button class="btn-clear">
+      <button class="btn-clear" title="Reset current time">
         <img alt="stop" src=${clearIcon} width=20 height=20 />
       </button>
-      <button class="btn-play">
+      <button class="btn-play" title="Play/Pause current time">
         <img class="img-play" alt="stop" src=${startIcon} width=20 height=20 />
       </button>
     </div>
@@ -26,10 +26,10 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
       <button class="switch-item">TIMER</button>
     </div>
     <div class="btn-group">
-      <button class="btn-pip">
+      <button class="btn-pip" title="Picture in Picture mode">
         <img alt="stop" src=${pipIcon} width=20 height=20 />
       </button>
-      <button class="btn-theme">
+      <button class="btn-theme" title="Toggle theme">
         <img alt="stop" src=${blackThemeIcon} width=20 height=20 />
       </button>
     </div>
